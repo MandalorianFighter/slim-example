70bbe6c67abf9e1120b97972bd44a1e1d63a8f07
MandalorianFighter <yanush.polishchuk@gmail.com>
2022-02-14 16:00:23 +0200

    add crud files

--- public/index.php
+++ public/index.php
@@ -20,96 +20,43 @@ $app->addErrorMiddleware(true, true, true);
 $app->add(MethodOverrideMiddleware::class);
 
 $router = $app->getRouteCollector()->getRouteParser();
+$repo = new Slim\Example\UserRepository();
 $vali = new Slim\Example\Validator();
-
-const EMAIL = 'admin@gmail.com';
-const PASS = 'qwerty123';
-
-function getUsers($request) {
-    return json_decode($request->getCookieParam('users') ?? '', true);
-}
 
 function filterByTerm($users, $term) {
     return array_filter($users, fn($user) => str_contains($user['nickname'], $term) !== false);
 }
 
-$app->get('/', function ($request, $response) use ($router) {
-    if(isset($_SESSION['isAdmin'])) {
-        return $response->withRedirect($router->urlFor('users'));
-    }
-
-    $messages = $this->get('flash')->getMessages();
-    $params = [
-        'email' => '',
-        'flash' => $messages
-    ];
-
-    return $this->get('renderer')->render($response, 'home.phtml', $params);
-})->setName('home');
-
-$app->post('/', function ($request, $response) use ($router) {
-
-    $user = $request->getParsedBodyParam('user');
-
-    $hash = password_hash(PASS, PASSWORD_DEFAULT);
-    $verify = password_verify($user['pass'], $hash);
-    
-        if ($user['email'] === EMAIL and $verify === true) {
-        $_SESSION['isAdmin'] = true;
-        $this->get('flash')->addMessage('success', 'Hey, Admin! You logged in successfully.');
-        return $response->withRedirect($router->urlFor('users'));
-    }
-    $this->get('flash')->addMessage('error', 'Access Denied!');
-    $params = [
-        'email' => $email,
-        'errors' => $errors
-    ];
-    $response = $response->withStatus(422);
-    return $response->withRedirect($router->urlFor('home'));
+$app->get('/', function ($request, $response) {
+    $response->getBody()->write('Welcome to Slim!');
+    return $response;
 });
 
-$app->delete('/logout', function ($request, $response) use ($router) {
-    session_destroy();
-    
-    return $response->withRedirect($router->urlFor('home'));
-});
-
-$app->get('/users', function ($request, $response) use ($router) {
-
-    if (!isset($_SESSION['isAdmin'])) {
-        $this->get('flash')->addMessage('error', 'Access Denied! Please login!');
-
-        return $response->withRedirect($router->urlFor('home'));
-    }
-
-    $users = getUsers($request);
+$app->get('/users', function ($request, $response) use ($repo) {
+    $users = $repo->all();
     $term = $request->getQueryParam('term');
 
     $usersList = $term ? filterByTerm($users, $term) : $users;
-
     $messages = $this->get('flash')->getMessages();
     $params = [
-        'term' => $term,
+	'term' => $term,
         'flash' => $messages,
         'users' => $usersList
     ];
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
 })->setName('users');
 
-$app->post('/users', function ($request, $response) use ($vali, $router) {
-
-    $users = getUsers($request);
+$app->post('/users', function ($request, $response) use ($repo, $vali) {
+    $users = $repo->all();
     $user = $request->getParsedBodyParam('user');
+    
     $errors = $vali->validate($user);
-
     if (count($errors) === 0) {
         $id = uniqid();
         $users[$id] = $user;
-
-        $encodedUsers = json_encode($users);
+        $repo->save($users);
         $this->get('flash')->addMessage('success', 'New user is created successfully');
-        return $response->withHeader('Set-Cookie', "users={$encodedUsers};Path=/")
-        ->withRedirect($router->urlFor('users'));
+        return $response->withRedirect('users', 302);
     }
     $params = [
         'user' => $user,
@@ -119,72 +66,45 @@ $app->post('/users', function ($request, $response) use ($vali, $router) {
     return $this->get('renderer')->render($response, "users/new.phtml", $params);
 });
 
-$app->get('/users/new', function ($request, $response) use ($router) {
-
-    if (!isset($_SESSION['isAdmin'])) {
-        $this->get('flash')->addMessage('error', 'Access Denied! Please login!');
-
-        return $response->withRedirect($router->urlFor('home'));
-    }
-
+$app->get('/users/new', function ($request, $response) {
     $params = [
         'user' => ['id' => '', 'nickname' => '', 'email' => ''],
         'errors' => []
     ];
     return $this->get('renderer')->render($response, "users/new.phtml", $params);
-})->setName('createUser');
+});
 
-$app->get('/users/{id}', function ($request, $response, $args) use ($router) {
-
-    if (!isset($_SESSION['isAdmin'])) {
-        $this->get('flash')->addMessage('error', 'Access Denied! Please login!');
-
-        return $response->withRedirect($router->urlFor('home'));
-    }
-    
-    $users = getUsers($request);
+$app->get('/users/{id}', function ($request, $response, $args) use ($repo) {
+    $users = $repo->all();
     $id = $args['id'];
-
-    $user = $users[$id];
-
-    $params = ['id' => $id, 'nickname' => $user['nickname'], 'email' => $user['email']];
-    if($user) {
-        return $this->get('renderer')->render($response, 'users/show.phtml', $params);
-    } else {
+    
+    if(!array_key_exists($id, $users)){
         return $response->withStatus(401)->write("Page not found");
     }
+    $user = $repo->find($id);
+    $params = ['id' => $id, 'nickname' => $user['nickname'], 'email' => $user['email']];
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 })->setName('user');
 
-$app->get('/users/{id}/edit', function ($request, $response, $args) use ($router) {
+$app->get('/users/{id}/edit', function ($request, $response, $args) use ($repo) {
 
-    if (!isset($_SESSION['isAdmin'])) {
-        $this->get('flash')->addMessage('error', 'Access Denied! Please login!');
-
-        return $response->withRedirect($router->urlFor('home'));
-    }
-    
     $id = $args['id'];
-    $users = getUsers($request);
-    $user = $users[$id];
-
+    $user = $repo->find($id);
     $messages = $this->get('flash')->getMessages();
     $params = [
-        'id' => $id,
+	'id' => $id,
         'flash' => $messages,
         'user' => $user,
         'errors' => []
             ];
-    if($user) {
         return $this->get('renderer')->render($response, 'users/edit.phtml', $params);
-    } else {
-        return $response->withStatus(401)->write("Page not found");
-    }
 })->setName('editUser');
 
-$app->patch('/users/{id}', function ($request, $response, array $args) use ($router, $vali)  {
+
+
+$app->patch('/users/{id}', function ($request, $response, array $args) use ($repo, $vali, $router)  {
     $id = $args['id'];
-    $users = getUsers($request);
-    $user = $users[$id];
+    $user = $repo->find($id);
     $data = $request->getParsedBodyParam('user');
 
     $errors = $vali->validate($data);
@@ -192,12 +112,15 @@ $app->patch('/users/{id}', function ($request, $response, array $args) use ($rou
     if (count($errors) === 0) {
         $user['nickname'] = $data['nickname'];
         $user['email'] = $data['email'];
-        $users[$id] = $user;
 
-        $encodedUsers = json_encode($users);
+        $users = $repo->all();
+        $users[$id] = $user;
+        $repo->save($users);
+
+        $this->get('flash')->addMessage('success', 'User has been updated');
+        
         $url = $router->urlFor('editUser', ['id' => $id]);
-        return $response->withHeader('Set-Cookie', "users={$encodedUsers};Path=/")
-        ->withRedirect($url);
+        return $response->withRedirect($url);
     }
 
     $params = [
@@ -210,14 +133,11 @@ $app->patch('/users/{id}', function ($request, $response, array $args) use ($rou
     return $this->get('renderer')->render($response, 'users/edit.phtml', $params);
 });
 
-$app->delete('/users/{id}', function ($request, $response, array $args) use ($router) {
-    $users = getUsers($request);
+$app->delete('/users/{id}', function ($request, $response, array $args) use ($repo, $router) {
     $id = $args['id'];
-    unset($users[$id]);
-    $encodedUsers = json_encode($users);
-    
+    $repo->remove($id);
     $this->get('flash')->addMessage('success', 'User has been deleted');
-    return $response->withHeader('Set-Cookie', "users={$encodedUsers};Path=/")->withRedirect($router->urlFor('users'));
+    return $response->withRedirect($router->urlFor('users'));
 });
 
 $app->run();
